94a502e6e4ef5a222f184a6d79c0fb53e2d7b5e3
Telecom <moussakonate27@gmail.com>
2018-03-05 17:13:05 +0100

    Added class Person with Setters and Getters

---
+++ src/org/moussa/serie04/exo11/Person.java
@@ -0,0 +1,28 @@
+package org.moussa.serie04.exo11;
+
+public class Person {
+	public String firstName;
+	public String lastName;
+	public int age;
+	
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+}
