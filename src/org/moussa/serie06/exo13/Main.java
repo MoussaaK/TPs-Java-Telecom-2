34cc5745e6fdc72c4c912a5a9de0825d92600ef2
Telecom <moussakonate27@gmail.com>
2018-03-12 16:33:26 +0100

    Removed unused import

--- src/org/moussa/serie06/exo13/Main.java
+++ src/org/moussa/serie06/exo13/Main.java
@@ -5,14 +5,8 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.OptionalInt;
 import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
-import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
-
-import com.sun.activation.registries.MailcapTokenizer;
 
 public class Main {
 
