5028238e0ee02fe1119c40e57c474320eaf5f87d
Telecom <moussakonate27@gmail.com>
2018-03-12 17:11:43 +0100

    Added main

--- src/org/moussa/serie06/exo14/Main.java
+++ src/org/moussa/serie06/exo14/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Germinal germinal = new Germinal();
+		System.out.println("Length of the Novel : " + germinal.linesOfGerminal("7germ10.txt").size());
+		//germinal.linesOfGerminal("7germ10.txt").forEach(s -> System.out.println(s));
+		
 	}
 
 }
