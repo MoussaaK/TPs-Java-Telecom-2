93ed964f1548890c00797ad496ec4dbe2236408b
Telecom <moussakonate27@gmail.com>
2018-03-12 17:11:20 +0100

    Added Germinal file and Germinal Class

---
+++ src/org/moussa/serie06/exo14/Germinal.java
@@ -0,0 +1,35 @@
+package org.moussa.serie06.exo14;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+public class Germinal {
+	
+	
+	
+	public List<String> readLinesFrom(String fileName) {
+		Path path = Paths.get(fileName);
+		try (Stream<String> lines = Files.lines(path)) {
+			return lines.collect(Collectors.toList());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+		}
+		return null;
+	}
+	
+	public List<String> linesOfGerminal(String fileName) {
+		List<String> lines = readLinesFrom(fileName);
+		lines.stream()
+			 .skip(70)
+			 .limit(lines.size() - 322)
+			 .collect(Collectors.toList());
+		
+		return lines;
+	}
+}
