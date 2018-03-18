a38d95a3e061ec19a87ce8aef06d721002b1928f
Moussee <moussakonate27@gmail.com>
2018-03-18 16:37:23 +0100

    Added new Bean Person, for exercise 14

---
+++ src/org/moussa/serie07/exo15/Person.java
@@ -0,0 +1,50 @@
+package org.moussa.serie07.exo15;
+
+public class Person {
+	public String firstName;
+	public String lastName;
+	public int age;
+	
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Person(String firstName, String lastName, int age) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
+	}
+
+}
