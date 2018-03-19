2643b97c70a64573fc740b5c12f07851aed0907f
Moussee <moussakonate27@gmail.com>
2018-03-19 22:25:24 +0100

    Updated Main to Test PerSonReader and PersonWriter

--- src/org/moussa/serie07/exo15/Main.java
+++ src/org/moussa/serie07/exo15/Main.java
@@ -1,6 +1,7 @@
 package org.moussa.serie07.exo15;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -20,8 +21,23 @@ public class Main {
 		}
 		listOfPerson.forEach(s -> System.out.println(s));
 		
+		List<Person> newListOfPerson = new ArrayList<>();
+		newListOfPerson.add(new Person("######", "######"));
+		newListOfPerson.add(new Person("Sundar", "Punchai", 45));
+		newListOfPerson.add(new Person("Jeff", "Bezos", 54));
+		newListOfPerson.add(new Person("Andi", "Rubin", 55));
+		
 		//added the already read listOfPerson to the File 
-		pw.write(listOfPerson, "Person.txt");
+		pw.write(newListOfPerson, "Person.txt");
+		
+		//
+		try {
+			listOfPerson = pr.read("Person.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		listOfPerson.forEach(s -> System.out.println(s));
 	}
 
 }
