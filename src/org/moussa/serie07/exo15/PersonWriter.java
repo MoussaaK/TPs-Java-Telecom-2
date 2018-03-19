775385aab6f6457c8cabe7f556d2f8909cdcd4a8
Telecom <moussakonate27@gmail.com>
2018-03-19 17:07:36 +0100

    Added Class PersonWriter to  write a Person inside the file Person.txt

--- src/org/moussa/serie07/exo15/PersonWriter.java
+++ src/org/moussa/serie07/exo15/PersonWriter.java
@@ -1,5 +1,8 @@
 package org.moussa.serie07.exo15;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.function.Function;
@@ -7,11 +10,21 @@ import java.util.function.Function;
 public class PersonWriter {
 	
 	Function<Person, String> personToLine =
-			(person) -> person.getFirstName() + ","
-											  + person.getLastName() + ","
-											  + person.getAge() + ",";
+			(person) -> "\n" + person.getFirstName() + ","
+													 + person.getLastName() + ","
+													 + person.getAge();
 		
 	public void write(List<Person> people, String fileName) throws IOException {
 		
+		File file = new File(fileName);
+		try (FileWriter fr = new FileWriter(file, true);
+				BufferedWriter br = new BufferedWriter(fr);) {
+			for (Person person : people) {
+				br.write(personToLine.apply(person));
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
