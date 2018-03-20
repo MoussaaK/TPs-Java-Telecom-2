22f7947ec385c4172578933b5c5a67b5f5649361
Telecom <moussakonate27@gmail.com>
2018-03-20 17:11:02 +0100

    Adde new Class to read binary File

---
+++ src/org/moussa/serie08/exo16/PersonWriter.java
@@ -0,0 +1,46 @@
+package org.moussa.serie08.exo16;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+
+public class PersonWriter {
+	
+	Function<Person, byte[]> personToBytes = person -> {
+		byte[] byteArray = null;
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			 DataOutputStream dos = new DataOutputStream(baos);) {
+			
+			dos.writeInt(person.getAge());
+			dos.writeUTF(person.getLastName());
+			dos.writeUTF(person.getFirstName());
+			byteArray = baos.toByteArray();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return byteArray;
+	};
+	
+	public void writeBinaryFields(List<Person> people, String fileName, int number) {
+		File file = new File(fileName);
+		try (FileOutputStream fos = new FileOutputStream(file, true);
+			 BufferedOutputStream bos = new BufferedOutputStream(fos);) {
+			bos.write(number);
+			for (Person person : people) {
+				byte [] bytes = personToBytes.apply(person);
+				bos.write(bytes);
+				bos.flush();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
