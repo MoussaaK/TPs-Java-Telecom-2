a04bacece378f0bf6829be4b62d62f3b4adfb5d9
Moussee <moussakonate27@gmail.com>
2018-03-26 01:15:35 +0200

    Updated Main to test the reading method of class PersonReader

--- src/org/moussa/serie08/exo16/Main.java
+++ src/org/moussa/serie08/exo16/Main.java
@@ -8,12 +8,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		PersonWriter pw = new PersonWriter();
+		PersonReader pr = new PersonReader();
+		String fileName = "files/BinaryPerson";
 		List<Person> people = new ArrayList<>();
-		people.add(new Person("######", "######"));
-		people.add(new Person("Sundar", "Punchai", 45));
+		//people.add(new Person("######", "######"));
+		people.add(new Person("Sundar", "Pichai", 45));
 		people.add(new Person("Jeff", "Bezos", 54));
 		people.add(new Person("Andi", "Rubin", 55));
 		
-		pw.writeBinaryFields(people, "files/BinaryPerson", people.size());
+		pw.writeBinaryFields(people, fileName, people.size());
+		List<Person> peopleRead = pr.readBinaryFields(fileName);
+		System.out.println("After reading the file... ");
+		System.out.println("Those People were found inside :");
+		peopleRead.forEach(p -> System.out.println(p));
 	}
 }
