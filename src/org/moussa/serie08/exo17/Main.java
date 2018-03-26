ed98a636b60bc1033ddb31d821e7a548e043a5de
Telecom <moussakonate27@gmail.com>
2018-03-26 16:07:45 +0200

    Added Main

---
+++ src/org/moussa/serie08/exo17/Main.java
@@ -0,0 +1,30 @@
+package org.moussa.serie08.exo17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Sundar", "Pichai", 45));
+		people.add(new Person("Jeff", "Bezos", 54));
+		people.add(new Person("Andi", "Rubin", 55));
+		people.add(new Person("Andi", "Rubin", 55));
+		
+		PersonWriter  pw = new PersonWriter();
+		pw.writeBinaryObject(people, "files/BinaryPerson2");
+		
+		PersonReader  pr= new PersonReader();
+		List<Person> peopleRead = pr.readBinaryObject("files/BinaryPerson2");
+		
+		for (Person person : peopleRead) {
+			System.out.println(person);
+		}
+		
+	}
+
+}
