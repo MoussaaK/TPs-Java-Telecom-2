024a85a1f1e9b6b0555b6436745c206dba8b5ec8
Telecom <Telecom@10.11.62.15>
2018-03-27 16:58:13 +0200

    Added Main to test methods

---
+++ src/org/moussa/serie09/exo18/Main.java
@@ -0,0 +1,29 @@
+package org.moussa.serie09.exo18;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AnalyzeBean bean = new AnalyzeBean();
+		Person person = new Person();
+		Employee employee = new Employee();
+		String fileName = "files/BeanToAnalyze";
+		Class<? extends Object> className = bean.getClassName(person);
+		System.out.println(className);
+		
+		Object clazz = null;
+		clazz = bean.getInstance(className.getName());
+		System.out.println(clazz);
+		
+		System.out.println("Properties of person  : " + bean.getProperties(person));
+		System.out.println("Properties of Employee  : " + bean.getProperties(employee));
+		
+		bean.set(person, "FirstName", "Moussa");
+		bean.set(person, "LastName", "Konate");
+		bean.set(person, "Age", 20);
+		
+		System.out.println(person);
+		bean.read(fileName);
+	}
+}
