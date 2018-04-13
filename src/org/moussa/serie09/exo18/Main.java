8bddd309a73c01c1be3551ce3bcfca2f88aba483
Moussee <moussakonate27@gmail.com>
2018-04-13 18:55:10 +0200

    Updated Main to test write method

--- src/org/moussa/serie09/exo18/Main.java
+++ src/org/moussa/serie09/exo18/Main.java
@@ -1,5 +1,7 @@
 package org.moussa.serie09.exo18;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) throws IllegalAccessException, ClassNotFoundException,
@@ -11,20 +13,25 @@ public class Main {
 		Employee employee = new Employee();
 		String fileName = "files/BeanToAnalyze";
 		Class<? extends Object> className = bean.getClassName(person);
-		System.out.println(className);
 		
-		Object clazz = null;
-		clazz = bean.getInstance(className.getName());
+		Object clazz = bean.getInstance(className.getName());
 		System.out.println(clazz);
 		
 		System.out.println("Properties of person  : " + bean.getProperties(person));
 		System.out.println("Properties of Employee  : " + bean.getProperties(employee));
 		
+		System.out.println("Testing set method...");
 		bean.set(person, "FirstName", "Moussa");
 		bean.set(person, "LastName", "Konate");
 		bean.set(person, "Age", 20);
-		
 		System.out.println(person);
-		System.out.println(bean.read(fileName));
+		
+		System.out.println("Testing read method...");
+		List<Object> beans = bean.read(fileName);
+		System.out.println(beans);
+		
+		System.out.println("Testing write method...");
+		bean.write(beans, "files/AnalyzedBeanOuput");
+		System.out.println("See content of 'file/AnalyzedBeanOuput' ...");
 	}
 }
